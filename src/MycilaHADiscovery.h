7ed835fc0237ea7d562c0eac4a3ac87fd432fa61
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-10-07 00:21:13 +0200

    v6.2.0

--- src/MycilaHADiscovery.h
+++ src/MycilaHADiscovery.h
@@ -10,9 +10,9 @@
 #include <vector>
 #include <memory>
 
-#define MYCILA_HA_VERSION          "6.1.0"
+#define MYCILA_HA_VERSION          "6.2.0"
 #define MYCILA_HA_VERSION_MAJOR    6
-#define MYCILA_HA_VERSION_MINOR    1
+#define MYCILA_HA_VERSION_MINOR    2
 #define MYCILA_HA_VERSION_REVISION 0
 
 #ifndef MYCILA_HA_DISCOVERY_TOPIC
